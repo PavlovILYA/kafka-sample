f8069dfa6be80896c94580a7821fcfdbe7adf0ca
Илюша <79216023478@yandex.ru>
2022-12-18 00:43:11 +0300

    feat: добавить Consumer'а

--- consumer/src/main/java/ru/pavlov/kafkasample/KafkaConsumerApplication.java
+++ consumer/src/main/java/ru/pavlov/kafkasample/KafkaConsumerApplication.java
@@ -1,10 +1,20 @@
 package ru.pavlov.kafkasample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
+@Slf4j
+@EnableKafka
 @SpringBootApplication
 public class KafkaConsumerApplication {
+
+	@KafkaListener(topics="message")
+	public void msgListener(String message){
+		log.info("Message from ExperimentalTopic: {}", message);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
