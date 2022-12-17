29d2e46ba4aaf3cc2ecf0a4147e017deb70c1db5
Илюша <79216023478@yandex.ru>
2022-12-17 23:39:26 +0300

    feat: добавить структуру проекта

---
+++ src/test/java/ru/pavlov/kafkasample/KafkaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pavlov.kafkasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
