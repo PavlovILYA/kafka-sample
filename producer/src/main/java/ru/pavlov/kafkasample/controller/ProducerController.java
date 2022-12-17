c7c8b4d657ac677c66089084ba18ab3a4b77d08b
Илюша <79216023478@yandex.ru>
2022-12-18 00:00:27 +0300

    feat: добавить Poducer'а refactor: разбить проект на модули

---
+++ producer/src/main/java/ru/pavlov/kafkasample/controller/ProducerController.java
@@ -0,0 +1,23 @@
+package ru.pavlov.kafkasample.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ProducerController {
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public void setKafkaTemplate(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("/send")
+    public void sendOrder(String msgId, String msg){
+        kafkaTemplate.send("message", msgId, msg);
+    }
+}
